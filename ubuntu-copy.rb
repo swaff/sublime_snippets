914d99f2b7b90c2f6c24974e10bcb1c75bb4867c
Mark Swaffer <mark.swaffer@nokia.com>
2012-10-10 13:59:55 +0100

    Added Ruby file to copy snippets
    - Copies files from current directory to ~/.config/sublime-text-2/Packages/User

---
+++ ubuntu-copy.rb
@@ -0,0 +1,14 @@
+# Copies the snippets from the current directory
+# to .config/sublime-text-2/Packages/User
+
+require 'fileutils'
+
+def should_copy(filename)
+  File.directory? filename and !filename.start_with?('.')
+end
+
+def copy(filename)
+  FileUtils.cp_r(filename, File.expand_path('~/.config/sublime-text-2/Packages/User'))
+end
+
+Dir.foreach(".") { |filename| copy(filename) if should_copy(filename) }
